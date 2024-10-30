0095b913a9f3b7f442c52c09a5d168aba6ec5afd
kickstartlab <info@kickstartlabs.io>
2024-10-30 21:25:43 +0500

    update favicon

--- src/components/Home.js
+++ src/components/Home.js
@@ -128,13 +128,13 @@ export default function Home() {
                             </div>
 
                             <div className='flex items-center justify-center gap-6 mt-5 '>
-                                <a href='https://x.com/pumpbysol'>
+                                <a href='https://x.com/pumpbysol' target="_blank">
                                     <button className='bg-blue-200 rounded-full py-2.5 px-4 hover:bg-blue-50 duration-150 hover:scale-110 shadow-btn1 w-24 border-4 border-yellow-100 hover:border-black-100'>
                                         <svg className='block mx-auto' xmlns="http://www.w3.org/2000/svg" width="1.4em" height="1.4em" viewBox="0 0 24 24"><path fill="#fff" d="M8 2H1l8.26 11.015L1.45 22H4.1l6.388-7.349L16 22h7l-8.608-11.478L21.8 2h-2.65l-5.986 6.886zm9 18L5 4h2l12 16z" /></svg>
                                     </button>
                                 </a>
 
-                                <a href='https://t.me/pumpbysol'>
+                                <a href='https://t.me/pumpbysol' target="_blank">
                                     <button className='bg-blue-200 rounded-full py-2.5 px-4 hover:bg-blue-50 duration-150 hover:scale-110 shadow-btn1 w-24 border-4 border-yellow-100 hover:border-black-100'>
                                         <svg className='block mx-auto' xmlns="http://www.w3.org/2000/svg" width="1.4em" height="1.4em" viewBox="0 0 24 24"><g fill="#fff" fillRule="evenodd"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="#fff" d="M19.777 4.43a1.5 1.5 0 0 1 2.062 1.626l-2.268 13.757c-.22 1.327-1.676 2.088-2.893 1.427c-1.018-.553-2.53-1.405-3.89-2.294c-.68-.445-2.763-1.87-2.507-2.884c.22-.867 3.72-4.125 5.72-6.062c.785-.761.427-1.2-.5-.5c-2.302 1.738-5.998 4.381-7.22 5.125c-1.078.656-1.64.768-2.312.656c-1.226-.204-2.363-.52-3.291-.905c-1.254-.52-1.193-2.244-.001-2.746z" /></g></svg>
                                     </button>
@@ -174,7 +174,7 @@ export default function Home() {
                                 </div>
 
                                 <div data-aos="fade-in" className='lg:w-1/2 w-full'>
-                                    <iframe title='wallet' src="https://jup.ag/swap/SOL-USDC" height="550" scrolling="no" className=' rounded-xl border-4 border-white-100 flex mx-auto lg:w-7/12'></iframe>
+                                    <iframe title='wallet' src="https://jup.ag/swap/SOL-USDC" height="670" className=' rounded-xl border-4 border-white-100 flex mx-auto lg:w-8/12 w-full'></iframe>
                                 </div>
                             </div>
 
@@ -745,7 +745,7 @@ export default function Home() {
                             </h3>
 
                             <div className='flex flex-wrap items-center gap-3 font-wedges'>
-                                <a href=' https://t.me/pumpbysol'>
+                                <a href=' https://t.me/pumpbysol' target="_blank">
                                     <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl md:tracking-wide md:text-base text-sm'>
                                         Telegram
                                     </button>
@@ -753,7 +753,7 @@ export default function Home() {
                                 <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl tracking-wide'>
                                     BUY $PUMP
                                 </button>
-                                <a href='https://x.com/pumpbysol'>
+                                <a href='https://x.com/pumpbysol' target="_blank">
                                     <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl md:tracking-wide md:text-base text-sm'>
                                         X | TWITTER
                                     </button>
