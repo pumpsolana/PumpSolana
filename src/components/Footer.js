5721fe17d4d0006ad62d7556af19bd43f8995572
kickstartlab <info@kickstartlabs.io>
2024-10-30 21:26:42 +0500

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,11 +30,11 @@ export default function Footer() {
           </nav>
 
           <div className='flex gap-x-8 md:my-5 items-center'>
-            <a href='https://x.com/pumpbysol'>
+            <a href='https://x.com/pumpbysol' target='_blank'>
               <svg className='block mx-auto' xmlns="http://www.w3.org/2000/svg" width="2.5em" height="2.5em" viewBox="0 0 24 24"><path fill="#b558ff" d="M8 2H1l8.26 11.015L1.45 22H4.1l6.388-7.349L16 22h7l-8.608-11.478L21.8 2h-2.65l-5.986 6.886zm9 18L5 4h2l12 16z" /></svg>
             </a>
 
-            <a href='https://t.me/pumpbysol'>
+            <a href='https://t.me/pumpbysol' target='_blank'>
               <i className='fa fa-telegram text-5xl text-yellow-50 duration-100'></i>
             </a>
           </div>
