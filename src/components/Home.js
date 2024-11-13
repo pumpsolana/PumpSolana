ba15d5b992e455248b60d419a3738ae8cc467660
pumpsolana <info@pumpsolana.xyz>
2024-11-13 16:53:02 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -749,9 +749,11 @@ export default function Home() {
                                         Telegram
                                     </button>
                                 </a>
-                                <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl tracking-wide'>
-                                    BUY $PUMP
-                                </button>
+                                <a href='https://app.meteora.ag/pools/5oWc1d4Z6sbrH75J4jSgCCwpgpmJwkpLQbNxiP15qGAA' target="_blank">
+                                    <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl tracking-wide'>
+                                        BUY $PUMP
+                                    </button>
+                                </a>
                                 <a href='https://x.com/pumpbysol' target="_blank">
                                     <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl md:tracking-wide md:text-base text-sm'>
                                         X | TWITTER
