c6c8a75111a760964f82ef31406ac4c449a3d054
pumpsolana <info@pumpsolana.xyz>
2024-11-13 17:04:44 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -744,14 +744,16 @@ export default function Home() {
                             </h3>
 
                             <div className='flex flex-wrap items-center gap-3 font-wedges'>
-                                <a href=' https://t.me/pumpbysol' target="_blank">
+                                <a href='https://t.me/pumpbysol' target="_blank">
                                     <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl md:tracking-wide md:text-base text-sm'>
                                         Telegram
                                     </button>
                                 </a>
-                                <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl tracking-wide'>
-                                    BUY $PUMP
-                                </button>
+                                <a href='https://app.meteora.ag/pools/5oWc1d4Z6sbrH75J4jSgCCwpgpmJwkpLQbNxiP15qGAA&outputaCurrency=sol' target="_blank">
+                                    <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl tracking-wide'>
+                                        BUY $PUMP
+                                    </button>
+                                </a>
                                 <a href='https://x.com/pumpbysol' target="_blank">
                                     <button className='shadow-btn bg-blue-200 border-4 border-yellow-50 hover:border-blue-50 py-2 px-4 text-white-100 hover:text-yellow-100 rounded-2xl md:tracking-wide md:text-base text-sm'>
                                         X | TWITTER
